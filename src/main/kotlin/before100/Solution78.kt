4457a4cbcf110234cef20ed896d3850baab0c21d
Igor Kozlov <kozlov.igor.dev@gmailcom>
2023-06-07 16:30:05 +0300

    78: solution without distinct

--- src/main/kotlin/before100/Solution78.kt
+++ src/main/kotlin/before100/Solution78.kt
@@ -5,26 +5,27 @@ class Solution78 {
     fun subsets(nums: IntArray): List<List<Int>> {
         val ans = mutableListOf<List<Int>>()
 
-        dfs(nums, ans, mutableListOf<Int>())
-        return ans
+        dfs(nums, ans, mutableListOf<Int>(), 0)
+        return ans//.distinct()
     }
 
-    private fun dfs(remainingNums: IntArray, ans: MutableList<List<Int>>, current: List<Int>) {
-        ans.add(current.toList())
-        if (remainingNums.isEmpty()) {
+    private fun dfs(nums: IntArray, ans: MutableList<List<Int>>, current: List<Int>, start: Int) {
+
+        if (start >= nums.size) {
+            ans.add(current)
             return
         }
 
 
-        val a = remainingNums.drop(1).toIntArray()
-        dfs(a, ans, current.plus(remainingNums[0]))
-        dfs(a, ans, current)
+        dfs(nums, ans, current.plus(nums[start]), start + 1)
+        dfs(nums, ans, current, start + 1)
 
     }
 
 
     companion object {
-        @JvmStatic fun main(args : Array<String>) {
+        @JvmStatic
+        fun main(args: Array<String>) {
             //add test
             //Input: nums = [1,2,3]
             //Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
