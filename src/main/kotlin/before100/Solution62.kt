b06b421d6fa1636db758b7046676732d28856841
Igor Kozlov <kozlov.igor.dev@gmailcom>
2023-06-07 23:48:38 +0300

    bottom up

--- src/main/kotlin/before100/Solution62.kt
+++ src/main/kotlin/before100/Solution62.kt
@@ -5,18 +5,18 @@ import other.TreeNode
 class Solution62 {
 
     fun uniquePaths(m: Int, n: Int): Int {
-        val dp = mutableMapOf((m - 1 to n - 1) to 1)
+        val dp = mutableMapOf((0 to 0) to 1)
 
         fun calc(x: Int, y: Int): Int {
             if (x < 0 || y < 0) return 0
             if (x >= m || y >= n) return 0
             if (dp.containsKey(x to y)) return dp[x to y]!!
 
-            dp[x to y] = calc(x + 1, y) + calc(x, y + 1)
+            dp[x to y] = calc(x - 1, y) + calc(x, y - 1)
             return dp[x to y]!!
         }
 
-        return calc(0, 0)
+        return calc(m - 1, n - 1)
     }
 
     companion object {
