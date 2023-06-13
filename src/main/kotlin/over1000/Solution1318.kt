57299dcb073e906275e7b2e54108755018fa18a0
Igor Kozlov <kozlov.igor.dev@gmailcom>
2023-06-13 11:49:36 +0300

    1318 solution without strings

--- src/main/kotlin/over1000/Solution1318.kt
+++ src/main/kotlin/over1000/Solution1318.kt
@@ -2,7 +2,11 @@ package over1000
 
 class Solution1318 {
 
-    fun minFlips(a: Int, b: Int, c: Int): Int {
+    /**
+     * Given 3 positives numbers a, b and c. Return the minimum flips required in some bits of a and b to make ( a OR b == c ). (bitwise OR operation).
+     * Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1 in their binary representation.
+     */
+    fun minFlips_v1(a: Int, b: Int, c: Int): Int {
 
         // c = 0 -> a = 0 && b = 0
         // c = 1 -> a = 1 || b = 0
@@ -23,11 +27,12 @@ class Solution1318 {
             val bChar = b2[i]
 
 
-            when(targetChar) {
+            when (targetChar) {
                 '0' -> {
                     if (aChar == '1') flips++
                     if (bChar == '1') flips++
                 }
+
                 '1' -> {
                     if (aChar == '0' && bChar == '0') flips++
                 }
@@ -37,14 +42,42 @@ class Solution1318 {
         return flips
     }
 
+    fun minFlips(a: Int, b: Int, c: Int): Int {
+        var flips = 0
+        var a1 = a
+        var b1 = b
+        var c1 = c
+
+        while (a1 > 0 || b1 > 0 || c1 > 0) {
+            val aBit = a1 and 1
+            val bBit = b1 and 1
+            val cBit = c1 and 1
+
+            when (cBit) {
+                0 -> {
+                    if (aBit == 1) flips++
+                    if (bBit == 1) flips++
+                }
+
+                1 -> {
+                    if (aBit == 0 && bBit == 0) flips++
+                }
+            }
+            a1 = a1 shr 1
+            b1 = b1 shr 1
+            c1 = c1 shr 1
+        }
+
+        return flips
+    }
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             //tests
             val s = Solution1318()
+            println(s.minFlips(4, 2, 7) == 1)
             println(s.minFlips(2, 6, 5))
-            println(s.minFlips(4, 2, 7))
             println(s.minFlips(1, 2, 3))
 
         }
