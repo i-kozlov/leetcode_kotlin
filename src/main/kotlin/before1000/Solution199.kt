b6cb631e773b3f255f6a32689bcbb63f76e58961
Igor Kozlov <kozlov.igor.dev@gmailcom>
2023-06-16 14:23:07 +0300

    199. Binary Tree Right Side View

--- src/main/kotlin/before1000/Solution199.kt
+++ src/main/kotlin/before1000/Solution199.kt
@@ -3,18 +3,33 @@ import other.TreeNode
 class Solution199 {
     fun rightSideView(root: TreeNode?): List<Int> {
         val list = mutableListOf<Int>()
-        var cur = root
-        while(cur != null){
-            list.add(cur.`val`)
-            cur = cur.right
+
+        fun traverse(root: TreeNode?, depth: Int){
+            if(root == null) return
+
+            if(list.size == depth){
+                list.add(depth, root.`val`)
+            }
+
+            traverse(root.right, depth + 1)
+            traverse(root.left, depth + 1)
         }
 
+        traverse(root, 0)
         return list
     }
 
 
     companion object {
         @JvmStatic fun main(args : Array<String>) {
+            //add example
+            val root = TreeNode(1)
+            root.left = TreeNode(2)
+            root.right = TreeNode(3)
+            root.left?.right = TreeNode(5)
+            root.right?.right = TreeNode(4)
+
+            println(Solution199().rightSideView(root))
 
         }
     }
