91ef192d7f90274d8e2a37bb2b0cd1caad1a374c
Igor Kozlov <kozlov.igor.dev@gmailcom>
2023-07-08 15:59:21 +0300

    303. Range Sum Query - Immutable

---
+++ src/main/kotlin/before1000/Solution303.kt
@@ -0,0 +1,36 @@
+package before1000
+
+import other.TreeNode
+
+class Solution303 {
+
+
+    class NumArray(nums: IntArray) {
+        private val prefixSums = IntArray(nums.size)
+
+        init {
+            prefixSums[0] = nums[0]
+            for(i in 1 .. nums.size - 1) {
+                prefixSums[i] = prefixSums[i-1] + nums[i]
+            }
+        }
+
+        fun sumRange(left: Int, right: Int): Int {
+
+            return prefixSums[right] - if(left == 0) 0 else prefixSums[left - 1]
+        }
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            //add example
+            //-2, 0, 3, -5, 2, -1
+            val nums = intArrayOf(-2, 0, 3, -5, 2, -1)
+            val numArray = NumArray(nums)
+            println(numArray.sumRange(0, 2))
+
+        }
+    }
+}
